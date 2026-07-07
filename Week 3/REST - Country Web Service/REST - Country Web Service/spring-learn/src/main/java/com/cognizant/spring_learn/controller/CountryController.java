7511bcff94a10b56bae5c9a129cee0d709be3783
BadmalakshmiViswanathan22 <23102020@rmd.ac.in>
2026-07-07 20:49:12 +0530

    Add files via upload

---
+++ Week 3/REST - Country Web Service/REST - Country Web Service/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,36 @@
+package com.cognizant.spring_learn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.model.Country;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        LOGGER.info("START");
+
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml")) {
+            Country country = (Country) context.getBean("country");
+
+            LOGGER.info("Country : {}", country.getName());
+
+            LOGGER.info("END");
+
+            return country;
+        } catch (BeansException e) {
+
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
