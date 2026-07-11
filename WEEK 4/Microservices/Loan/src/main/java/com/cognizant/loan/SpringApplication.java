7768a37dfea7160f580b478cb4269efd45f2d9a1
BadmalakshmiViswanathan22 <23102020@rmd.ac.in>
2026-07-11 09:41:34 +0530

    Add files via upload

---
+++ WEEK 4/Microservices/Loan/src/main/java/com/cognizant/loan/SpringApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.cognizant.loan;
+
+/**
+ * SpringApplication
+ */
+public class SpringApplication {
+
+    public static void run(Class<LoanApplication> class1, String[] args) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+}
